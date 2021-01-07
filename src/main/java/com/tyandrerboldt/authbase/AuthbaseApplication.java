cdba254530908af23ae38cb1acda2bf76711db0c
Tyandrer Boldt <tyandrer.boldt@gmail.com>
2021-01-07 19:07:17 -0300

    Create project

---
+++ src/main/java/com/tyandrerboldt/authbase/AuthbaseApplication.java
@@ -0,0 +1,13 @@
+package com.tyandrerboldt.authbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthbaseApplication.class, args);
+	}
+
+}
