e902c77456997b7668003d6719eada8d909d6dc0
Tyandrer Boldt <tyandrer.macedo@cepedi.org.br>
2021-01-08 17:00:09 -0300

    Add properties to User model

--- src/main/java/com/tyandrerboldt/authbase/domain/models/User.java
+++ src/main/java/com/tyandrerboldt/authbase/domain/models/User.java
@@ -2,17 +2,41 @@ package com.tyandrerboldt.authbase.domain.models;
 
 import java.time.OffsetDateTime;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Entity(name = "tb_user")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Column(nullable = false)
 	private String name;
 	
+	@Column(nullable = false)
 	private String email;
 	
+	@Column(nullable = false)
 	private String password;
 	
+	@CreationTimestamp
+	@Column(nullable = false, columnDefinition = "datetime")
 	private OffsetDateTime createdAt;
 	
+	@UpdateTimestamp
+	@Column(nullable = false, columnDefinition = "datetime")
 	private OffsetDateTime updatedAt;
 }
