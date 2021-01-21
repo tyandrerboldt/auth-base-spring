50de87dd287ab5f046aeb50b41a8a6a11241fb2e
Tyandrer Boldt <tyandrer.boldt@gmail.com>
2021-01-21 20:36:39 -0300

    Change lib to OAuth2

--- src/main/java/com/tyandrerboldt/authbase/core/security/WebSecurityConfig.java
+++ src/main/java/com/tyandrerboldt/authbase/core/security/WebSecurityConfig.java
@@ -3,10 +3,8 @@ package com.tyandrerboldt.authbase.core.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -20,20 +18,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.withUser("admin")
 				.password(passwordEncoder().encode("123456"))
 				.roles("ADMIN");			
-	}
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.httpBasic()
-			.and()
-			.authorizeRequests()
-				.antMatchers("/v1/public/**").permitAll()
-				.anyRequest().authenticated()
-			.and()
-				.sessionManagement()
-					.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-			.and()
-				.csrf().disable();
 	}
 	
 	@Bean
---
+++ src/main/java/com/tyandrerboldt/authbase/core/security/authorizationserver/AuthorizationServerConfig.java
@@ -0,0 +1,11 @@
+package com.tyandrerboldt.authbase.core.security.authorizationserver;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+}
