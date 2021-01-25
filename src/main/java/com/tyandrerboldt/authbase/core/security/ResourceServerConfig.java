0219143fcc561ecfe21e27f0b5e1b420c04752e6
Tyandrer Boldt <tyandrer.boldt@gmail.com>
2021-01-25 20:54:46 -0300

    Refact Authorization code using defaul form control

--- src/main/java/com/tyandrerboldt/authbase/core/security/ResourceServerConfig.java
+++ src/main/java/com/tyandrerboldt/authbase/core/security/ResourceServerConfig.java
@@ -3,20 +3,27 @@ package com.tyandrerboldt.authbase.core.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @Configuration
-@EnableResourceServer
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-			.anyRequest().authenticated()
-		.and()
-		.oauth2ResourceServer().jwt();
+		http
+			.formLogin()
+			.and()
+			.authorizeRequests()
+				.antMatchers("/oauth/**").authenticated()
+			.and()
+			.csrf().disable()
+			.cors().and()
+			.oauth2ResourceServer().jwt();
 	}	
 	
 	@Bean
